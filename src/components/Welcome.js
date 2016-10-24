cc35d08c267de697c76a98ca5d0955654f9c3829
vasanth-mahendran <vasanth.mahendran@objectedge.com>
2016-10-23 19:55:58 -0500

    Added Charts

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,67 +1,74 @@
 import React from 'react';
 import { Grid, Row, Col, Button, PageHeader, Glyphicon, Carousel, Navbar, Nav, 
   NavDropdown, MenuItem  } from 'react-bootstrap';
-import ReactTable from 'react-table';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import {Pie} from 'react-chartjs-2';
 
 export default class Welcome extends React.Component {
   constructor() {
     super();
     this.state = { 
-      items: [],
+      sliderItems: [],
+      tableItems: [],
       index:0,
       direction:null,
-      columns:[
-        {
-          header:'Name',
-          accessor: 'name'
-        },
-        {
-          header:'Food',
-          accessor: 'food'
-        },
-        {
-          header:'Travel',
-          accessor: 'travel'
-        },
-        {
-          header:'Accomodation',
-          accessor: 'accomodation'
-        },
-        {
-          header:'Entertaiment',
-          accessor: 'entertaiment'
-        },
-        {
-          header:'Emergency',
-          accessor: 'emergency'
-        },
-        {
-          header:'Others',
-          accessor: 'other'
-        }
-      ]
+      pieIndex:0,
+      pieDirection:null,
+      pieSliderItems: []
     };
-    this.handleRefresh = this.handleRefresh.bind(this);
+    this.colors = ['#F7464A','#46BFBD','#FDB45C','#949FB1','#4D5360','#9dd49d'];
+    this.highlights = ['#FF5A5E','#5AD3D1','#FFC870','#A8B3C5','#616774','#4ca24c'];
+    this.labels = ['Food','Travel','Accomodation','Entertaiment','Emergency','Other']
+    this.handleSliderRefresh = this.handleSliderRefresh.bind(this);
+    this.handleTableRefresh = this.handleTableRefresh.bind(this);
+    this.handlePieSliderRefresh = this.handlePieSliderRefresh.bind(this);
     this.handleSelect = this.handleSelect.bind(this);
+    this.handlePieSelect = this.handlePieSelect.bind(this);
     this.renderNavbarMenu = this.renderNavbarMenu.bind(this);
+    this.getChartData = this.getChartData.bind(this);
   }
+
   handleSelect(selectedIndex, e) {
     this.setState({
       index: selectedIndex,
       direction: e.direction
     });
   }
-  handleRefresh(){
+  handlePieSelect(selectedIndex, e) {
+    this.setState({
+      pieIndex: selectedIndex,
+      pieDirection: e.direction
+    });
+  }
+
+  handleSliderRefresh(){
     fetch('/getexpenditure')
     .then(response => response.json())
     .then((json)=> {
-      this.setState({items:json.expenditures});
+      this.setState({sliderItems:json.expenditures});
     });
   }
+
+  handleTableRefresh(){
+    fetch('/getexpenditure')
+    .then(response => response.json())
+    .then((json)=> {
+      this.setState({tableItems:json.expenditures});
+    });
+  }
+
+  handlePieSliderRefresh(){
+    fetch('/getexpenditure')
+    .then(response => response.json())
+    .then((json)=> {
+      this.setState({pieSliderItems:json.expenditures});
+    });
+  }
+
   renderNavbarMenu() {
     const title = <strong><span className="glyphicon glyphicon-menu-hamburger"></span></strong>;
     return (
-      <Nav className="navbar-top-links" pullRight style={{backgroundColor:'#2b336b'}}>
+      <Nav className="navbar-top-links" pullRight>
         <NavDropdown title={title} noCaret style={{padding: 0}} id="userDropDown">
           <MenuItem href='#slider'>Slider</MenuItem>
           <MenuItem href='#table'>Table</MenuItem>
@@ -70,13 +77,22 @@ export default class Welcome extends React.Component {
       </Nav>
     );
   }
+  getChartData(item,idx){
+    var data = {};
+    var keys = Object.keys(item);
+    keys.splice(0,1);
+    var values = keys.map(function(v) { return item[v]; });
+    data.labels = this.labels;
+    data.datasets = [{data:values,backgroundColor:this.colors,hoverBackgroundColor:this.highlights}]
+    return data;
+  }
   render () {
     var carouselContent;
-    if(this.state.items.length==0){
+    if(this.state.sliderItems.length==0){
       carouselContent = <Carousel.Item>
               <div style={{height:'300px',width:'100%',textAlign:'center'}}>
-                <Button type="button" className="btn btn-default" onClick={this.handleRefresh}>
-                  <Glyphicon glyph='refresh'/>
+                <Button type="button" className="btn btn-default" onClick={this.handleSliderRefresh}>
+                  <Glyphicon glyph='refresh' style={{fontSize: '30px'}}/>
                 </Button>
               </div>
               <Carousel.Caption>
@@ -85,7 +101,7 @@ export default class Welcome extends React.Component {
               </Carousel.Caption>
             </Carousel.Item>;
     }else{
-      carouselContent = this.state.items.map((item,idx)=>
+      carouselContent = this.state.sliderItems.map((item,idx)=>
       {
         return <Carousel.Item>
               <div style={{height:'300px',width:'100%',textAlign:'center','verticalAlign':'middle'}}>
@@ -105,11 +121,47 @@ export default class Welcome extends React.Component {
       }
       )
     }
+
+    var pieCarouselContent;
+    if(this.state.pieSliderItems.length==0){
+      pieCarouselContent = <Carousel id='chart' activeIndex={this.state.pieIndex} direction={this.state.pieDirection} onSelect={this.handlePieSelect}
+          className='jumbotron'>
+              <Carousel.Item>
+                <div style={{height:'300px',width:'100%',textAlign:'center'}}>
+                  <Button type="button" className="btn btn-default" onClick={this.handlePieSliderRefresh}>
+                    <Glyphicon glyph='refresh' style={{fontSize: '30px'}}/>
+                  </Button>
+                </div>
+                <Carousel.Caption>
+                  <h3>Expenditure Data</h3>
+                  <p>Click the refresh button to know the expenditure data of your friends</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>;
+    }else{
+      pieCarouselContent = this.state.pieSliderItems.map((item,idx)=>
+      {
+        var data = this.getChartData(item,idx);
+        return <Carousel.Item>
+                <div style={{height:'650px',width:'100%',textAlign:'center','verticalAlign':'middle'}}>
+                  <Pie data={data}/>
+                </div>
+                <Carousel.Caption>
+                  <h3>Expenditure data</h3>
+                  <p>Check the expenditures of {item.name}</p>
+                </Carousel.Caption>
+              </Carousel.Item>;
+      }
+      )
+      pieCarouselContent = <Carousel id='chart' style={{height:'700px'}} activeIndex={this.state.pieIndex} direction={this.state.pieDirection} onSelect={this.handlePieSelect}
+          className='jumbotron'>{pieCarouselContent}</Carousel>
+    }
+
     function priceFormatter(cell, row){
       return '<i class="glyphicon glyphicon-usd"></i> ' + cell;
     }
     return(
-      <div id="page-wrapper" className="gray-bg">
+      <div id="page-wrapper" className="gray-bg" style={{marginBottom:'20px'}}>
         <div className="navigation-header-wrapper">
           <Row className="border-bottom">
             <Navbar className="navbar-static-top" bsStyle="default" style={{ marginBottom: 0, height: 60 }}>
@@ -129,14 +181,47 @@ export default class Welcome extends React.Component {
           </Row>
         </div>
         <Grid>
+          <Row>
+            <Col xs={12}>
+            <h2>Sliders</h2>
+            <p>To represent Expenditure data is as sliders,I have used Bootstrap controlled carousel.Refer 
+            http://react-bootstrap.github.io/components.html#carousels for more detail about bootstrap carousel</p>
+            </Col>
+          </Row>
           <Carousel id='slider' activeIndex={this.state.index} direction={this.state.direction} onSelect={this.handleSelect}
           className='jumbotron'>
             {carouselContent}
           </Carousel>
-          <ReactTable
-            data={this.state.items}
-            columns={this.state.columns}
-          />
+          <Row>
+            <Col xs={12}>
+            <h2>Tables</h2>
+            <p>To represent Expenditure data is as tables,I have used react-bootstrap-table module.Refer 
+            https://github.com/AllenFang/react-bootstrap-table for more detail about react-bootstrap-table</p>
+            </Col>
+          </Row>
+          <div id='table'>
+            <BootstrapTable data={this.state.tableItems} striped={true} hover={true}
+            options={ { noDataText: <Button type="button" className="btn btn-default" onClick={this.handleTableRefresh}>
+                    <Glyphicon glyph='refresh' style={{fontSize: '30px'}}/>
+                  </Button> } }>
+                <TableHeaderColumn dataField="name" isKey={true} dataAlign="center" dataSort={true}>Name</TableHeaderColumn>
+                <TableHeaderColumn dataField="food" dataFormat={priceFormatter} dataSort={true}>Food</TableHeaderColumn>
+                <TableHeaderColumn dataField="travel" dataFormat={priceFormatter} dataSort={true}>Travel</TableHeaderColumn>
+                <TableHeaderColumn dataField="accomodation" dataFormat={priceFormatter} dataSort={true}>Accomodation</TableHeaderColumn>
+                <TableHeaderColumn dataField="entertaiment" dataFormat={priceFormatter} dataSort={true}>Entertaiment</TableHeaderColumn>
+                <TableHeaderColumn dataField="emergency" dataFormat={priceFormatter} dataSort={true}>Emergency</TableHeaderColumn>
+                <TableHeaderColumn dataField="other" dataFormat={priceFormatter} dataSort={true}>Others</TableHeaderColumn>
+            </BootstrapTable>
+          </div>
+          <Row>
+            <Col xs={12}>
+            <h2>Charts</h2>
+            <p>To represent Expenditure data is as charts,I have used react-bootstrap-table module.Refer 
+            https://github.com/AllenFang/react-bootstrap-table for more detail about react-bootstrap-table</p>
+            </Col>
+          </Row>
+          
+          {pieCarouselContent}
         </Grid>
       </div>
       );
